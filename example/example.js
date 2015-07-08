13660c7a331936938b1a9870fcf200b3a1cee4cf
Yasar icli <hello@yasaricli.com>
2015-07-08 14:49:05 +0300

    example mp3 removed upload to fileer.com

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
   Template.player.events({
     'click .play': function() {
-      Player.play('/example.mp3');
+      Player.play('http://www.fileer.com/cfs/files/files/X9wbajyE4c8aPQQpE/g2DjCigBDpZy8MgTDKWXMpFAtLfJB8QLGDQ2QNhmeJuJFwBsrk.mp3');
     },
 
     'click .stop': function() {
